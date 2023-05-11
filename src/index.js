34bccc6ade8aaf7f6d9a3e4c16513492358ef1e3
Arely Gonzalez <arely.web@gmail.com>
2023-05-11 10:52:33 -0600

    modifique las validaciones de nombre, cvv y fecha de caducidad

--- src/index.js
+++ src/index.js
@@ -9,20 +9,35 @@ document.getElementById("idbotonRealizarPago").addEventListener("click", functio
 
 function valida(numTarjeta) { 
 
-  const tarjetaLimpia = limpiaYvalidaNum(numTarjeta);
+  const nombre = document.getElementById("idnombre").value; 
+  const fechaCaducidad = document.getElementById("idfechaCaducidad").value; 
+  const cvv = document.getElementById("idcvv").value; 
+
+
+  const tarjetaLimpia = limpiaYvalidaNum(numTarjeta,nombre,fechaCaducidad,cvv);
 
   if(tarjetaLimpia !== 0){
 
     const RespValida = validator.isValid(tarjetaLimpia);
 
     if (RespValida!==true){
+      document.getElementById("msg_g").style.display="none";
+      document.getElementById("msg_m").style.display="none";
+      document.getElementById("msg_b").style.display="";
       document.getElementById("msg_b").innerHTML = "Número inválido.";
+   
     }else{
+      document.getElementById("msg_b").style.display="none";
+      document.getElementById("msg_m").style.display="none";
+      document.getElementById("msg_g").style.display="";
       document.getElementById("msg_g").innerHTML = "Se realizó el pago.";
     }
      
   }else{
-    document.getElementById("msg_b").innerHTML = "Número inválido.";
+    document.getElementById("msg_g").style.display="none";
+    document.getElementById("msg_b").style.display="none";
+    document.getElementById("msg_m").style.display="";
+    document.getElementById("msg_m").innerHTML="Verifique los datos ingresados";
   }
  
   
@@ -38,12 +53,17 @@ function mask(numTarjeta){
 /**
     * funcion que limpia el numero de tarjeta para no tener espacios y validar tamaño
     */
-function limpiaYvalidaNum (num) {
+function limpiaYvalidaNum (num,nom,fv,cvv) {
+
+  const vfv = fv.split("/");
+  
   const expRegSoloNum = "^[0-9]+$"; //expresion regular que verifica que sean solo numeros
   const numSinEspacios = num.replace(/\s/g, ''); // remplaza los espacios por ´´ de manera global
- 
-  if (numSinEspacios.match(expRegSoloNum) !== null ){
+  const anio = new Date().getFullYear().toString().substr(-2);
+
+  if (numSinEspacios.match(expRegSoloNum) !== null && nom !== "" && vfv[1] >=anio && cvv !== ""){
     return numSinEspacios;
+
   }else { return 0;}
 }
 
